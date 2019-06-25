52c26871a7d3e42f020d414e16a44f2249cfd64c
miguelpduarte <miguelpduarte98@gmail.com>
2019-06-25 01:30:28 +0100

    Fixed some problems left behind by refactor (missing import, react spaghet by not using hooks, etc)

--- src/components/StopPageRoot.jsx
+++ src/components/StopPageRoot.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Layout from "./Layout";
 import SEO from "./Seo";
 import { getInfoFromStopId } from "../utils/parsing";
 import StopSchedule from "./StopSchedule";
+import SimplePaperMessage from "./SimplePaperMessage";
 
 const StopPageRoot = ({stop_id}) => {
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-
-    let provider_name, stop_name;
-    try {
-        [provider_name, stop_name] = getInfoFromStopId(stop_id);
-    } catch (ignored) {
-        // Ignoring the error variable as it can only be a parsing error due to the regex in getInfoFromStopId not applying
-        // The value of the error variable is not relevant as well, since this component only handles that one error, and a boolean value is more than enough
-        setError(true);
-    }
+    const [stop_info, setStopInfo] = useState({provider_name: "", stop_name: ""});
+    
+    useEffect(() => {
+        try {
+            const [provider_name, stop_name] = getInfoFromStopId(stop_id);
+            setError(false);
+            setStopInfo({
+                provider_name,
+                stop_name
+            });
+            setLoading(false);
+        } catch (ignored) {
+            // Ignoring the error variable as it can only be a parsing error due to the regex in getInfoFromStopId not applying
+            // The value of the error variable is not relevant as well, since this component only handles that one error, and a boolean value is more than enough
+            setError(true);
+            setLoading(false);            
+        }
+    }, [stop_id]);
 
     return (
         <Layout>
-            <SEO title={error ? "Invalid Stop ID" : `Schedule for ${stop_name}`} />
+            <SEO title={error ? "Invalid Stop ID" : `Schedule for ${stop_info.stop_name}`} />
             <>
-                {error ?
-                    <SimplePaperMessage message={"Invalid Stop ID"}/>
+                {loading ?
+                    <React.Fragment/>
                     :
-                    <StopSchedule stop_name={stop_name} provider_name={provider_name} stop_id={stop_id} />
+                    error ?
+                        <SimplePaperMessage message={"Invalid Stop ID"}/>
+                        :
+                        <StopSchedule stop_name={stop_info.stop_name} provider_name={stop_info.provider_name} stop_id={stop_id} />
                 }
             </>
         </Layout>
