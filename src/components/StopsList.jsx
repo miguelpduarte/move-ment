772b810c9d6942fa005f70c050d1f22e8edbfec4
miguelpduarte <miguelpduarte98@gmail.com>
2019-06-30 17:02:59 +0100

    Fixed sideways scroll in stop search page for mobile

--- src/components/StopsList.jsx
+++ src/components/StopsList.jsx
@@ -3,8 +3,17 @@ import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
 import StopListItem from "./StopListItem";
 import SimplePaperMessage from "./SimplePaperMessage";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+    listItem: {
+        marginBottom: theme.spacing(2),
+    },
+}));
 
 const StopsList = ({stops, no_items}) => {
+    const classes = useStyles();
+
     return (
         <Grid container justify="center" spacing={3}>
             {stops ?
@@ -14,7 +23,7 @@ const StopsList = ({stops, no_items}) => {
                     </Grid>
                     :
                     stops.map(stop => (
-                        <Grid key={stop.stop_id} item xs={12}>
+                        <Grid key={stop.stop_id} item xs={12} className={classes.listItem}>
                             <StopListItem stop={stop} />
                         </Grid>
                     ))
