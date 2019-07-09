7eb6248f09283cdaedb33a5d2ef88250f62ecec7
miguelpduarte <miguelpduarte98@gmail.com>
2019-07-09 19:58:53 +0100

    Improved imports

--- src/components/ClearableSearchBar.jsx
+++ src/components/ClearableSearchBar.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import InputBase from "@material-ui/core/InputBase";
-import IconButton from "@material-ui/core/IconButton";
-import Divider from "@material-ui/core/Divider";
+import { Paper, InputBase, IconButton, Divider } from "@material-ui/core";
 import { Search as SearchIcon, Clear as ClearIcon } from "@material-ui/icons";
 
 const useStyles = makeStyles({
