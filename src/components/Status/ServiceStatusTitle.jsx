d4af5cb618c836203652da3058cdece2c5eeadfb
miguelpduarte <miguelpduarte98@gmail.com>
2019-07-09 22:19:02 +0100

    Added slight bottom margin to the service title in the status page, will also help with giving the loading bar a bit more breathing space

--- src/components/Status/ServiceStatusTitle.jsx
+++ src/components/Status/ServiceStatusTitle.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        marginBottom: theme.spacing(1),
+    }
+}));
 
 const ServiceStatusTitle = ({name}) => {
+    const classes = useStyles();
+
     return (
-        <Typography variant="h4">
+        <Typography className={classes.root} variant="h4">
             {name}:
         </Typography>
     );
