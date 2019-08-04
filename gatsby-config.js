9b0c3a9826301942f965ecae17fdbe04f9b13fbe
miguelpduarte <miguelpduarte98@gmail.com>
2019-08-04 22:45:03 +0100

    Add app environment checks in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,15 +18,16 @@ module.exports = {
         {
             resolve: "gatsby-plugin-manifest",
             options: {
-                name: "move-ment",
-                short_name: "move-ment",
+                name: process.env.APP_ENVIRONMENT === "production" ? "move-ment" : "move-ment beta",
+                short_name: process.env.APP_ENVIRONMENT === "production" ? "move-ment" : "move-ment beta",
                 start_url: "/",
                 background_color: "#00897b",
                 theme_color: "#00897b",
                 // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
                 // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
                 display: "standalone",
-                icon: "src/images/Bus_stop_symbol.png", // This path is relative to the root of the site.
+                // TODO: Make image square and add different one for non-prod environment (beta dev)
+                icon: process.env.APP_ENVIRONMENT === "production" ? "src/images/Bus_stop_symbol.png" : "src/images/Bus_stop_symbol.png", // This path is relative to the root of the site.
                 // An optional attribute which provides support for CORS check.
                 // If you do not provide a crossOrigin option, it will skip CORS for manifest.
                 // Any invalid keyword or empty string defaults to `anonymous`
@@ -37,8 +38,8 @@ module.exports = {
         {
             resolve: "gatsby-plugin-google-analytics",
             options: {
-                // Setting either the tracking id for prod or for beta (Dev) websites
-                trackingId: process.env.NODE_ENV === "production" ? "UA-137730755-3" : "UA-137730755-4",
+                // Setting either the tracking id for prod or for beta (dev) websites
+                trackingId: process.env.APP_ENVIRONMENT === "production" ? "UA-137730755-3" : "UA-137730755-4",
                 head: true,
                 anonymize: true,
                 respectDNT: true,
